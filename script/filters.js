947f7a0b9e728fb52085d970871c5757b0f7f8bb
Omar Rabie <omar.rabie@hotmail.fr>
2023-07-12 23:34:29 +0200

    HTML avec bootstrap

---
+++ script/filters.js
@@ -0,0 +1,46 @@
+// const selectBox1 = document.querySelector(".select-box1");
+// const selectOption1 = document.querySelector(".select-option1");
+// const soValue1 = document.querySelector("#soValue1");
+// const optionSearch1 = document.querySelector("#optionSearch1");
+// const options1 = document.querySelector(".options1");
+// const optionsList1 = document.querySelectorAll(".options1 li");
+
+// const selectBox2 = document.querySelector(".select-box2");
+// const selectOption2 = document.querySelector(".select-option2");
+// const soValue2 = document.querySelector("#soValue2");
+// const optionSearch2 = document.querySelector("#optionSearch2");
+// const option2 = document.querySelector(".options2");
+// const optionsList2 = document.querySelectorAll(".options2 li");
+
+// const selectBox3 = document.querySelector(".select-box3");
+// const selectOption3 = document.querySelector(".select-option3");
+// const soValue3 = document.querySelector("#soValue3");
+// const optionSearch3 = document.querySelector("#optionSearch3");
+// const option3 = document.querySelector(".options3");
+// const optionsList3 = document.querySelectorAll(".options3 li");
+
+// // adding a listener whenever a click on the selecOption1 
+// selectOption1.addEventListener("click" , function(){
+//     selectBox1.classList.toggle("active");
+// })
+
+// // choose an item from the list
+// optionsList1.forEach(function(optionListSingle){
+//     optionListSingle.addEventListener("click", function(){
+//         text = this.textContent;
+//         soValue1.value = text;
+//         selectBox1.classList.remove("active");
+//     })
+// })
+
+// // adding a listener whenever a click on the selecOption2
+// selectOption2.addEventListener("click" , function(){
+//     selectBox2.classList.toggle("active");
+// })
+
+// // adding a listener whenever a click on the selecOption3
+// selectOption3.addEventListener("click" , function(){
+//     selectBox3.classList.toggle("active");
+// })
+
+
